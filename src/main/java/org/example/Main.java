a0566dd4a67614fc4a13771538397cd61675e493
JavaFaraon <ivan@rybakovka.com>
2023-03-09 16:24:18 +0200

    finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().run();
+    }
+
+    private void run() {
+
+        Solution solution = new Solution();
+        Solution2 solution2 = new Solution2();
+        ListNode list1 = new ListNode(1);
+        //ListNode list2 = new ListNode(1);
+
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+
+
+        list1.printList();
+
+        ListNode reverseList = solution2.reverseList(list1);
+        System.out.println("Result: ");
+        reverseList.printList();
     }
 }
--- src/main/java/org/example/Solution2.java
+++ src/main/java/org/example/Solution2.java
@@ -1,2 +1,16 @@
-package org.example;public class Solution2 {
+package org.example;
+
+public class Solution2 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode rest = reverseList(head.next);
+        rest.printList();
+        head.next.next = head;
+        head.next = null;
+
+        return rest;
+    }
 }
